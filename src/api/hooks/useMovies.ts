1bf99febdf414a14607f4bb16fdbf78fcb7dbea9
choimy <75023251+striveperson@users.noreply.github.com>
2026-04-03 01:44:34 +0900

    feat useMovieListByGenre combine을 통해 data, isPending 값 정의

--- src/api/hooks/useMovies.ts
+++ src/api/hooks/useMovies.ts
@@ -11,6 +11,12 @@ export const useGenres = (req: SearchRequest) => useQuery(movieQueries.genres(re
 
 export const useMovieListByGenre = (genreIds: number[], req: SearchRequest) => useQueries({
   queries: genreIds.map(id => ({ ...movieQueries.movieListByGenre(id, req), enabled: genreIds.length > 0 })),
+  combine: (results) => {
+    return {
+      data: results.map(result => result.data?.data.results.slice(0, 10) ?? []),
+      isPending: results.some(result => result.isPending),
+    }
+  }
 });
 
 export const useMovieDetail = (movieId: number, req: SearchRequest) => useQuery(movieQueries.movieDetail(movieId, req));

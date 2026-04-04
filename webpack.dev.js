34774acaa47e86b591575a8fc843d8d888e08320
choimy <75023251+striveperson@users.noreply.github.com>
2026-04-04 22:31:08 +0900

    fix - webpack common내 환경별 세팅 적용한 내용 각 환경에 맞는 파일로 이전

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,24 @@
 const { merge } = require("webpack-merge");
 const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map', // 개발 모드에서 디버깅 편의를 위해 소스맵 생성
+  devtool: 'eval-source-map', // 개발 모드에서 디버깅 편의를 위해 소스맵 생성
+  plugins: [
+    new Dotenv({
+      path: './.env.development'
+    }), // env 파일을 로드하여 process.env.xxx 형태로 사용 가능하게,
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  },
+  
   devServer: { // 개발 서버
     proxy: [{ // 로컬에서 tmdb API 호출 시 CORS 문제 해결 위해 프록시 설정
       context: ['/api'], // '/api'로 시작하는 요청 가로챔

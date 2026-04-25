22d91d195727c4271fa47e9db9349d75531877d6
choimy <75023251+striveperson@users.noreply.github.com>
2026-04-26 01:32:43 +0900

    fix font 라이브러리 추가에 따른 파일 수정

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,6 +2,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 
 import { ModalProvider } from '@components/modal/ModalContext';
+import '@fontsource/noto-sans-kr/400.css';
+import '@fontsource/noto-sans-kr/700.css';
+import '@fontsource/noto-sans-kr/900.css';
 import Router from './router';
 
 const queryClient = new QueryClient();
